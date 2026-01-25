4bdd7c08189cb3e5c9fa813d6a7cd4394541842d
Thabiso <epema.thabiso@gmail.com>
2026-01-25 07:34:43 +0000

    Remove manual service worker

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("sw.js", { scope: "/" })
-      .then((reg) => console.log("Service worker registered!", reg))
-      .catch((err) => console.log("Service worker registration failed:", err));
-  });
-}
-
 import "./style.css";
 import words from "./words.json";
 
